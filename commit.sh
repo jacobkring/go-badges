8c28349d42392099d380f1051f1883cb419eafa9
Jacob Kring <jacob.alan.kring@gmail.com>
2021-05-16 00:02:38 -0400

    see if the file is actually changed

--- commit.sh
+++ commit.sh
@@ -3,6 +3,7 @@
 git config --global user.name "$(git --no-pager log --format=format:'%an' -n 1)"
 git config --global user.email "$(git --no-pager log --format=format:'%ae' -n 1)"
 cd /github/workspace
+cat README.md
 git add README.md
 git commit -m $INPUT_COMMIT-MSG
 git push
