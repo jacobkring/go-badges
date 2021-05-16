b1595bb1f99580df662d150309d742108e87f9d5
Jacob Kring <jacob.alan.kring@gmail.com>
2021-05-16 13:54:25 -0400

    #patch, weird that the report card output isn't passed correctly

--- commit.sh
+++ commit.sh
@@ -4,7 +4,8 @@ git config --global user.name "$(git --no-pager log --format=format:'%an' -n 1)"
 git config --global user.email "$(git --no-pager log --format=format:'%ae' -n 1)"
 cd /github/workspace
 cat README.md
-if [ GITHUB_ACTIONS == "true" ] ; then
+
+if [ $GITHUB_ACTIONS == "true" ] ; then
   git add README.md
   git commit -m $INPUT_COMMIT-MSG
   git push
