793fe944b459dd9089a02562c2856fc3da354695
Jacob Kring <jacob.alan.kring@gmail.com>
2021-05-16 20:34:35 -0400

    see if this works

--- commit.sh
+++ commit.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+
+source=${SOURCE:-.}
+cd ${GITHUB_WORKSPACE}/${source}
+
 git config --global user.name "$(git --no-pager log --format=format:'%an' -n 1)"
 git config --global user.email "$(git --no-pager log --format=format:'%ae' -n 1)"
-cd /github/workspace
-cat README.md
+git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY
+git checkout "${GITHUB_REF:11}"
+
+echo $GITHUB_ACTIONS
 
 if [ $GITHUB_ACTIONS == "true" ] ; then
   git add README.md
