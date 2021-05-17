093ba2dfe44d7e0f0ee510c193ca7fcbc4cf97cb
Jacob Kring <jacob.alan.kring@gmail.com>
2021-05-16 20:37:17 -0400

    tweak

--- commit.sh
+++ commit.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 
-source=${SOURCE:-.}
-cd ${GITHUB_WORKSPACE}/${source}
 
-git config --global user.name "$(git --no-pager log --format=format:'%an' -n 1)"
-git config --global user.email "$(git --no-pager log --format=format:'%ae' -n 1)"
-git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY
-git checkout "${GITHUB_REF:11}"
 
 echo $GITHUB_ACTIONS
+echo $GITHUB_TOKEN
 
 if [ $GITHUB_ACTIONS == "true" ] ; then
+  source=${SOURCE:-.}
+  cd ${GITHUB_WORKSPACE}/${source}
+
+  git config --global user.name "$(git --no-pager log --format=format:'%an' -n 1)"
+  git config --global user.email "$(git --no-pager log --format=format:'%ae' -n 1)"
+  git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY
+  git checkout "${GITHUB_REF:11}"
   git add README.md
   git commit -m $INPUT_COMMIT-MSG
   git push
