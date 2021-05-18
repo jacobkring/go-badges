990a6aec21d5fdc2f2a6fdd801fc03f221fc5d69
Jacob Kring <jacob.alan.kring@gmail.com>
2021-05-18 02:10:13 -0400

    branch issue?

--- commit.sh
+++ commit.sh
@@ -1,14 +1,7 @@
 #!/bin/sh -l
 
-echo "github actions"
-echo $GITHUB_ACTIONS
-echo "github token"
-echo $GITHUB_TOKEN
-echo $INPUT_COMMIT_MSG
-
 echo "---- github workspace"
-ls ${GITHUB_WORKSPACE}/${source}
-
+echo ${GITHUB_REF#refs/heads/}
 echo "------"
 
 if [ $GITHUB_ACTIONS == "true" ] ; then
@@ -18,7 +11,7 @@ if [ $GITHUB_ACTIONS == "true" ] ; then
   git config --global user.name "$(git --no-pager log --format=format:'%an' -n 1)"
   git config --global user.email "$(git --no-pager log --format=format:'%ae' -n 1)"
   git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY
-  git checkout "${GITHUB_REF:11}"
+  git checkout "${GITHUB_REF#refs/heads/}"
   git add README.md
   git commit -m "go-badges update"
   git push
