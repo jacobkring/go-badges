d408ace16d6c73038c494403e295b266cf903bc6
jacobkring <jacobkring@users.noreply.github.com>
2021-05-19 16:21:50 -0400

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -7,7 +7,8 @@ echo "------"
 if [ $GITHUB_ACTIONS == "true" ] ; then
   source=${SOURCE:-.}
   cd ${GITHUB_WORKSPACE}/${source}
-
+  
+  git remote -v
   git config --global user.name "$(git --no-pager log --format=format:'%an' -n 1)"
   git config --global user.email "$(git --no-pager log --format=format:'%ae' -n 1)"
   git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY
