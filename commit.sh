63868b43233ac3217134ba64a7a8520a7776de37
jacobkring <jacobkring@users.noreply.github.com>
2021-05-19 15:59:21 -0400

    dont checkout main

--- commit.sh
+++ commit.sh
@@ -11,7 +11,7 @@ if [ $GITHUB_ACTIONS == "true" ] ; then
   git config --global user.name "$(git --no-pager log --format=format:'%an' -n 1)"
   git config --global user.email "$(git --no-pager log --format=format:'%ae' -n 1)"
   git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY
-  git checkout "${GITHUB_REF#refs/heads/}"
+  # git checkout "${GITHUB_REF#refs/heads/}"
   git add README.md
   git commit -m "go-badges update"
   git push
